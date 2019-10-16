3412f1d62c7fea8cabf878aa21c8e2d3ecafac73
xzt1995 <49546812+xzt1995@users.noreply.github.com>
2019-10-16 13:42:11 +0800

    idea 测试 github

---
+++ nideshop-server/src/main/java/com/newland/nideshopserver/mapper/AddressMapper.java
@@ -0,0 +1,13 @@
+package com.newland.nideshopserver.mapper;
+
+import com.newland.nideshopserver.config.MyMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author xzt
+ * @create 2019-10-16 13:40
+ */
+@Mapper
+public interface AddressMapper extends MyMapper {
+
+}
