bb89e5e1e5cc1dcf476495366c492b2cf541ea73
LaoWang <1014193810@qq.com>
2019-10-21 14:25:15 +0800

    处理冲突

--- nideshop-server/src/main/java/com/newland/nideshopserver/service/impl/CommentServiceImpl.java
+++ nideshop-server/src/main/java/com/newland/nideshopserver/service/impl/CommentServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.ISelect;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.newland.nideshopserver.mapper.CommentMapper;
 import com.newland.nideshopserver.mapper.CommentPictureMapper;
 import com.newland.nideshopserver.mapper.UserMapper;
@@ -33,69 +36,72 @@ public class CommentServiceImpl implements CommentService {
 	private CommentPictureMapper commentPictureMapper;
 
 	@Override
-	public CountSelect listService(int typeId, int valueId, int size) throws Exception {
-		// page = (page > 0 ) ? page : 1 ;
-		size = (size > 0) ? size : 20;
-		// showType = (showType > 0 ) ? showType : 0 ;
-		int page = 1;
-		int showType = 0;
-		List<NideshopComment> commentList = null;
-		// 当前topic评论总数
-		int count = 0;
-		if (showType == 0) {
-			// 全部评论数量
-			count = commentMapper.commentCount(typeId, valueId);
-			// 查询TOPIC所有评论数据（取前size个）
-			Example e = new Example(NideshopComment.class);
-			Example.Criteria c = e.createCriteria();
-			c.andEqualTo("typeId", typeId);
-			c.andEqualTo("valueId", valueId);
-			List<NideshopComment> comments = commentMapper.selectByExample(e);
-			if (comments != null) {
-				commentList = comments.subList(0, size);
-			}
-		} else {
-			// 带图片评论数量
-			count = commentMapper.joinCommentCount(typeId, valueId);
-			commentList = commentMapper.picComment(typeId, valueId, page, size);
-		}
-		ArrayList<Comment> list = new ArrayList<>();
-		if (commentList != null) {
-			for (NideshopComment c : commentList) {
-				Comment comment = new Comment();
-				// base64 解码
-				String content = Utis.base64Decoder(c.getContent());
-				comment.setContent(content);
-				comment.setTypeId(c.getTypeId());
-				comment.setValueId(c.getValueId());
-				comment.setId(c.getId());
-				// 时间转码
-				comment.setAddTime(Utis.timeFormat(c.getAddTime()));
-				// 查询该条评论用户的信息
-				Example e = new Example(NideshopUser.class);
-				e.selectProperties("username", "avatar", "nickname");
-				Example.Criteria criteria = e.createCriteria();
-				criteria.andEqualTo("id", c.getUserId());
-				NideshopUser user = userMapper.selectOneByExample(e);
-				comment.setUserInfo(user);
-				// 查询评论图片信息
-				Example e1 = new Example(NideshopCommentPicture.class);
-				e1.createCriteria().andEqualTo("commentId", c.getId());
-				List<NideshopCommentPicture> pictures = commentPictureMapper.selectByExample(e1);
-				comment.setPicList(pictures);
-				list.add(comment);
-			}
-		}
-		// 分页总页数
-		int totalPages = Utis.totalPages(count, size);
-		CountSelect countSelect = new CountSelect();
-		countSelect.setData(list);
-		countSelect.setCount(count);
-		countSelect.setCurrentPage(page);
-		countSelect.setPageSize(size);
-		countSelect.setTotalPages(totalPages);
-		return countSelect;
-	}
+    public CountSelect listService(int typeId, int valueId, int size) throws Exception {
+        // page = (page > 0 ) ? page : 1 ;
+        size = (size > 0) ? size : 20;
+        // showType = (showType > 0 ) ? showType : 0 ;
+        int page = 1;
+        int showType = 0;
+        List<NideshopComment> commentList = null;
+        // 当前topic评论总数
+        int count = 0;
+        if (showType == 0) {
+            // 全部评论数量
+            count = commentMapper.commentCount(typeId, valueId);
+            // 查询TOPIC所有评论数据（取前size个）
+            Example e = new Example(NideshopComment.class);
+            Example.Criteria c = e.createCriteria();
+            c.andEqualTo("typeId", typeId);
+            c.andEqualTo("valueId", valueId);
+            PageInfo<NideshopComment> pageInfo = PageHelper.startPage(page, size).doSelectPageInfo(new ISelect() {
+                @Override
+                public void doSelect() {
+                    commentMapper.selectByExample(e);
+                }
+            });
+           commentList = pageInfo.getList();
+        } else {
+            // 带图片评论数量
+            count = commentMapper.joinCommentCount(typeId, valueId);
+            commentList = commentMapper.picComment(typeId, valueId, page, size);
+        }
+        ArrayList<Comment> list = new ArrayList<>();
+        if (commentList != null) {
+            for (NideshopComment c : commentList) {
+                Comment comment = new Comment();
+                // base64 解码
+                String content = Utis.base64Decoder(c.getContent());
+                comment.setContent(content);
+                comment.setTypeId(c.getTypeId());
+                comment.setValueId(c.getValueId());
+                comment.setId(c.getId());
+                // 时间转码
+                comment.setAddTime(Utis.timeFormat(c.getAddTime()));
+                // 查询该条评论用户的信息
+                Example e = new Example(NideshopUser.class);
+                e.selectProperties("username", "avatar", "nickname");
+                Example.Criteria criteria = e.createCriteria();
+                criteria.andEqualTo("id", c.getUserId());
+                NideshopUser user = userMapper.selectOneByExample(e);
+                comment.setUserInfo(user);
+                // 查询评论图片信息
+                Example e1 = new Example(NideshopCommentPicture.class);
+                e1.createCriteria().andEqualTo("commentId", c.getId());
+                List<NideshopCommentPicture> pictures = commentPictureMapper.selectByExample(e1);
+                comment.setPicList(pictures);
+                list.add(comment);
+            }
+        }
+        // 分页总页数
+        int totalPages = Utis.totalPages(count, size);
+        CountSelect countSelect = new CountSelect();
+        countSelect.setData(list);
+        countSelect.setCount(count);
+        countSelect.setCurrentPage(page);
+        countSelect.setPageSize(size);
+        countSelect.setTotalPages(totalPages);
+        return countSelect;
+    }
 
 	@Override
 	public int allCount(int typeId, int valueId) {
